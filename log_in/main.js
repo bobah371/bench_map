e9bec1f4f5ac3411b574f47771d182b32fcd2005
Mikhail <mihazizgan79@gmail.com>
2021-04-24 19:18:07 +0300

    modified add

---
+++ log_in/main.js
@@ -0,0 +1,18 @@
+function checkParams() {
+    var name = $('#login').val();
+    var password = $('#password').val();
+     
+
+    const button = document.getElementById('submit')
+
+    if(name.length != 0  && password.length != 0) {
+        //$('#submit').removeAttr('disabled');
+        button.disabled = false;
+       // $('#submit').removeAttr('disabled');
+        console.log('yes')
+    } else {
+        button.disabled = true;
+        //$('#submit').attr('disabled');
+        console.log('no')
+    }
+}
