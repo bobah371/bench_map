7640c32b4bb53339b8ac907dc801dc8d927c661a
arskell <burgline1221@gmail.com>
2021-04-24 17:40:55 +0300

    update server

--- server/nodejs/main.js
+++ server/nodejs/main.js
@@ -40,11 +40,126 @@ var addPlace = (x_pos, y_pos, type, user_id, path, description) =>{
 };
 
 
-var findUser = (login, password) => { 
-	return 'SELECT login, hash_pass FROM ' + users_table + ' WHERE login = \'' +login+ '\' and hash_pass = \''+getHashStr(password)+ '\'';
+var findUserWithRawPassword = (login, password) => { 
+	return 'SELECT * FROM ' + users_table + ' WHERE login = \'' +login+ '\' and hash_pass = \''+getHashStr(password)+ '\'';
 };
 
+var findUserWithoutHash = (login, password) => { 
+	return 'SELECT login, hash_pass FROM ' + users_table + ' WHERE login = \'' +login+ '\' and hash_pass = \''+password+ '\'';
+};
+
+
 const ON_ERROR_RESPONSE = 'Ooops, error';
+
+/*словарь команд*/
+var actions = new Map();
+
+
+actions.set('/loadpoints', async (req, res, query) =>{
+	
+	const x = query['x']
+	const y = query['y']
+	const radius = query['radius']
+		
+	if(x != null && y!= null && radius!=null){
+		
+		try{
+				
+			res.writeHead(200, { 'Content-Type': 'application/json' });
+			db = await client.query(getPoints(x, y, radius) );
+			res.write(JSON.stringify(db['rows']));
+		
+		}catch (err){
+			console.log(err);
+			res.writeHead(400);
+		}
+	}else{
+		res.writeHead(400);
+	}
+});
+
+actions.set('/registration', async (req, res, query) => {
+	
+		const login = query['login'];
+		const email = query['email'];
+		const password = query['password'];
+			
+		if(login != null && email != null && password != null){
+				
+			try{
+			
+				db = await client.query(addUser(login, email, password) );
+				console.log('Registration user ' + login);
+				res.writeHead(303, {
+				'Location': 'signin/signed.html'
+				});
+		
+			}catch (err){
+				console.log(err);
+				res.writeHead(400);
+			}	
+				
+			}else{
+				res.writeHead(400);
+		}
+	
+});
+
+actions.set('/login', async (req, res, query) => {
+	
+		const login = query['login'];
+		const password = query['password'];
+	
+		if(login == null || password == null){
+			res.writeHead(404);
+			return;
+		}
+		
+		try{
+			
+			var user_id = '';
+			var pass_hash = '';
+
+			db = await client.query(findUserWithRawPassword(login, password), async (err, qres) =>{
+				
+				try{
+					
+					const id = qres['rows'][0]['id'];
+					const hash_pass = qres['rows'][0]['hash_pass'];
+					
+					
+					console.log('login user ' + login);
+					
+					res.writeHead(303, {
+						'Location': 'signin/signed.html'
+					});
+					
+					
+					//res.writeHead(303, {
+						//'Set-Cookie': 'user_id='+id,
+						//'Set-Cookie': 'hash_pass='+hash_pass,
+					//	'Location': 'main/index.html'
+					//});
+					
+				}catch(err){
+					/*cannot find a user*/
+					console.log(err);
+					res.writeHead(400);
+					return;
+				}
+				
+				
+			});
+			
+		}catch(err){
+			/*database error*/
+			res.writeHead(400);
+			return;
+		}
+	
+});
+
+
 
 const requestListener = async function (req, res) {
 
@@ -53,60 +168,19 @@ const requestListener = async function (req, res) {
 		const HREF = JSON.parse(JSON.stringify(hrefObject));
 		
         const queryObject = url.parse(req.url.toString(),true).query;
-        const obj = JSON.parse(JSON.stringify(queryObject));
+        const query = JSON.parse(JSON.stringify(queryObject));
 		
 		const action = HREF.split('?')[0];
 		
 		console.log(action);
 		
-		if(action == '/loadpoints'){
-			/*загрузить точки*/
-			const x = obj['x']
-			const y = obj['y']
-			const radius = obj['radius']
-		
-			if(x != null && y!= null && radius!=null){
-		
-			try{
-				
-				res.writeHead(200, { 'Content-Type': 'application/json' });
-				db = await client.query(getPoints(x, y, radius) );
-				res.write(JSON.stringify(db['rows']));
-		
-			}catch (err){
-				console.log(err);
-				res.writeHead(400);
-			}
-			}else{
-				res.writeHead(400);
-			}
-		}else if(action == '/registration'){
-			/*регистрация в аккаунте*/
-			const login = obj['login'];
-			const email = obj['email'];
-			const password = obj['password'];
-			
-			if(login != null && email != null && password != null){
-				
-				try{
-			
-					db = await client.query(addUser(login, email, password) );
-					console.log('Registration user ' + login);
-					res.writeHead(303/*2*/, {
-					'Location': 'signin/signed.html'
-					//add other headers here...
-					});
-		
-				}catch (err){
-					console.log(err);
-					res.writeHead(400);
-				}
-				
-				
-			}else{
-				res.writeHead(400);
-			}
-			
+		try{
+			f = actions.get(action);
+			await f(req, res, query);
+
+		}catch(err){
+			res.writeHead(404);
+			console.log(err);
 		}
 		res.end();
 
