77100a55d8f4e884bd9b940aa3686d65e2fe3a3e
Mikhail <mihazizgan79@gmail.com>
2021-04-25 12:02:42 +0300

    added description

--- main/icon_customImage.js
+++ main/icon_customImage.js
@@ -23,4 +23,21 @@ ymaps.ready(function () {
 	});
 	
 
+    myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
+        hintContent: 'Собственный значок метки',
+        balloonContent: 'Это красивая метка'
+    }, {
+        // Опции.
+        // Необходимо указать данный тип макета.
+        iconLayout: 'default#image',
+        // Своё изображение иконки метки.
+        iconImageHref: 'images/myIcon.gif',
+        // Размеры метки.
+        iconImageSize: [30, 42],
+        // Смещение левого верхнего угла иконки относительно
+        // её "ножки" (точки привязки).
+        iconImageOffset: [-5, -38]
+    }),
+
+
 });
